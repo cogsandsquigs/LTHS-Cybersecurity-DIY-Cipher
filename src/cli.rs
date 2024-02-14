2714b5734aeb4099431b2aca2dd7892fa12d8e5b
Ian Pratt <ipratt@cps.edu>
2024-02-14 08:13:33 -0800

    asdf

--- src/cli.rs
+++ src/cli.rs
@@ -1,14 +1,48 @@
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 
 /// Encrypts and decrypts a string using my own DIY cipher
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
-struct Args {
-    /// Name of the person to greet
-    #[arg(short, long)]
-    name: String,
+pub struct Args {
+    /// Action to take
+    #[command(subcommand)]
+    pub action: Action,
 
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+    /// The key to use. Can be any string
+    #[arg(short = 'k', long, required = true)]
+    pub key: String,
+
+    /// The size of the key to use. Either 256, 384, or 512
+    #[arg(short = 's', long, value_enum, default_value = "256")]
+    pub key_size: KeySize,
+}
+
+/// The different key sizes available
+#[derive(ValueEnum, Debug, Clone)]
+pub enum KeySize {
+    /// 256 bits
+    #[value(name = "256")]
+    Bits256 = 256,
+
+    /// 384 bits
+    #[value(name = "384")]
+    Bits384 = 384,
+
+    /// 512 bits
+    #[value(name = "512")]
+    Bits512 = 512,
+}
+
+/// The action to take.
+#[derive(Parser, Debug)]
+pub enum Action {
+    /// Encrypt a string
+    Encrypt {
+        /// The string to encrypt
+        #[arg(required = true)]
+        input: String,
+    },
+
+    /// Decrypt a string
+    Decrypt {},
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,36 @@
 mod cli;
 
+use clap::Parser;
+use cli::{Action, Args, KeySize};
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match args.action {
+        Action::Encrypt { input } => encrypt(args.key, input, args.key_size),
+        Action::Decrypt {} => todo!(),
+    }
+}
+
+fn encrypt(key: String, input: String, key_size: KeySize) {
+    println!("Encrypting input with a {}-bit key", key_size as u16);
+
+    // Convert the input to 16-byte blocks and key to a byte array
+    let key = key.as_bytes();
+    let input: Vec<Vec<u8>> = input
+        .as_bytes()
+        .chunks(16)
+        .map(|chunk| {
+            if chunk.len() == 16 {
+                chunk.to_vec()
+            } else {
+                let mut new_chunk = chunk.to_vec();
+                new_chunk.resize(16, 0);
+                new_chunk
+            }
+        })
+        .collect();
+
+    println!("Key: {:?}", key);
+    println!("Input: {:?}", input);
 }
