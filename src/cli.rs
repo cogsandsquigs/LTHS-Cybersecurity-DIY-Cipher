cbd69e5c47059523efa348bebcd02bf2b9320d9b
Ian Pratt <ipratt@cps.edu>
2024-02-14 07:57:55 -0800

    asdf

---
+++ src/cli.rs
@@ -0,0 +1,14 @@
+use clap::Parser;
+
+/// Encrypts and decrypts a string using my own DIY cipher
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    name: String,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
